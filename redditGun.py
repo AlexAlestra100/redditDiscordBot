2d1043e14c455357d7592663f944ba8cde3f3472
AlexAlestra100 <kestrel.alestra@gmail.com>
2024-09-05 21:30:30 +0000

    Code import

---
+++ redditGun.py
@@ -0,0 +1,81 @@
+import requests
+from bs4 import BeautifulSoup
+import discord
+from discord.ext import commands, tasks
+from dotenv import load_dotenv
+import os
+import time
+
+load_dotenv()
+
+config = {
+    "TOKEN": os.getenv('TOKEN'),
+    "CHANNEL_ID": int(os.getenv('CHANNEL_ID')),
+    "USER_ID": os.getenv('USER_ID'),
+    "URL": os.getenv('URL'),
+    "KEYWORDS": os.getenv('KEYWORDS').split(','),
+    "OTHER_KEYWORDS": os.getenv('OTHER_KEYWORDS').split(',')
+}
+
+CACHE = []
+CACHE_EXPIRY = 15 * 60  # 15 minutes in seconds
+
+def scrape_reddit():
+    print('Scrapping Data...')
+    url = config['URL']
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    title_elements = soup.find_all('a', class_='block font-semibold text-neutral-content-strong m-0 visited:text-neutral-content-weak text-16 xs:text-18 mb-2xs xs:mb-xs')
+
+    keywords = config['KEYWORDS']
+    otherKeywords = config['OTHER_KEYWORDS']
+
+    titles = []
+    base_url = "https://www.reddit.com"
+    for title_element in title_elements:
+        if title_element.has_attr('slot') and title_element['slot'] == 'title':
+            title = title_element.text
+            if any(keyword.lower() in title.lower() for keyword in keywords):
+                if any(otherKeyword.lower() in title.lower() for otherKeyword in otherKeywords):
+                    href = title_element.get('href')
+                    if not href.startswith(base_url):
+                        href = base_url + href
+                    titles.append({'title': title, 'link': href})
+    return titles
+
+def is_post_alerted(post):
+    current_time = time.time()
+    for timestamp, cached_post in CACHE:
+        if current_time - timestamp < CACHE_EXPIRY and cached_post['link'] == post['link']:
+            return True
+    return False
+
+def add_post_to_cache(post):
+    CACHE.append((time.time(), post))
+
+class MyClient(commands.Bot):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.msg_sent = False
+
+    async def on_ready(self):
+        channel = bot.get_channel(config['CHANNEL_ID'])
+        await self.watcher.start(channel)
+
+    @tasks.loop(seconds=20)
+    async def watcher(self, channel):
+        posts = scrape_reddit()
+        if posts:
+            for post_data in posts:
+                if not is_post_alerted(post_data):
+                    user_id = config['USER_ID']
+                    message = f"{user_id} \nTitle: {post_data['title']} \nLink: {post_data['link']}"
+                    await channel.send(message)
+                    add_post_to_cache(post_data)
+                else:
+                    print('Post already alerted.')
+            print('Message Sent.')
+
+
+bot = MyClient(command_prefix='', intents=discord.Intents.all())
+bot.run(config['TOKEN'])
